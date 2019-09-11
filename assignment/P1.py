b48f1086c73f6d67a58c3a2a3f79e6c1c2914f85
shilpa <shilparpatil86@gmail.com>
2019-09-11 11:56:24 +0530

    assign

---
+++ assignment/P1.py
@@ -0,0 +1,31 @@
+i = 1
+for i in range(1,11):
+    print(i,end=' ')
+print(' ')
+j = 5
+for j in range(5,26):
+    print(j,end=' ')
+print(' ')
+k = 1
+for k in range(1,6):
+    print(5*k,end=' ')
+print(' ')
+k = 1
+for k in range(1,51):
+    print(2*k,end=' ')
+print(' ')
+k = 1
+for k in range(1,100):
+    if k % 2 != 0:
+        print(k, end=' ')
+print(' ')
+
+for k in range(25,-1,-5):
+    print(k, end=' ')
+
+
+
+
+
+
+
---
+++ assignment/Practise1.py
@@ -0,0 +1,8 @@
+print('hi')
+s = 'hi'
+s1 = s.upper()
+print(s1)
+s2 = "Hello"
+print(s2.lower())
+print(s2.islower())
+print(s1.isupper())
