adc51ab0e88028f364aae7a7ee95265841dba5a0
shreyas <shreyassharma143@gmail.com>
2019-09-11 14:47:01 +0530

    Added Assignemnt file

---
+++ Assignment 1.py
@@ -0,0 +1,14 @@
+str = 'python for life'
+print('printing first o/p')
+print(str[0:6])
+print(str[7:11])
+print(str[11:15])
+print(str[2:11])
+print(str[8:13])
+print(' ')
+print('printing second o/p')
+print(str[0:10],str[11:15].upper())
+print('')
+
+print(('printing third o/p'))
+print(str[0:6].upper(), str[7:10], str[11:15].upper())
