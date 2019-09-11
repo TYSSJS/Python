ee6e74a32178f196a3e9da46f88d93f3105577eb
salonipattnaik <52771346+salonipattnaik@users.noreply.github.com>
2019-09-11 11:37:10 +0530

    Add files via upload

---
+++ PyFile.py
@@ -0,0 +1,21 @@
+# print("hello")
+from itertools import count
+
+l=[1, 23, 'hello', 98.6, 98, 23]
+print(l[1:3])
+print(l[0:6:2])
+print(l[0:6:3])
+s="hi"
+print(s.upper())
+s="H_Ello"
+print(s.lower())
+print(s.isupper())
+print(s.islower())
+print(s.casefold())
+txt="manisha"[::-1]
+print(txt)
+print(len("afifa anjum"))
+print(s.__lt__("_"))
+print(txt.count("a"))
+print(txt.rfind("a", 0, 6))
+
