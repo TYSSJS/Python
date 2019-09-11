c2215424969b5a46010cc682915d7db47de0a5a7
afifaanjum20 <52885517+afifaanjum20@users.noreply.github.com>
2019-09-11 11:36:49 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,39 @@
+a = 1
+b = 10
+
+for num in range(a, b + 1):
+    print(num,end=" ")
+print(" ")
+c=5
+d=25
+for num in range(c,d+1):
+    print(num,end=" ")
+print(" ")
+
+#even numbers
+e=2
+f=100
+for num in range(e,f+1,2):
+    print(num,end=" ")
+print(" ")
+
+#odd numbers
+g=1
+h=30
+for num in range(g,h+1,2):
+    print(num,end=" ")
+print(" ")
+
+#multiple of 5
+i=5
+j=25
+for num in range(i,j+1,5):
+    print(num,end=" ")
+print(" ")
+#multiple of 5 in revrse
+k=5
+l=25
+for num in range(l,k-1,-5):
+    print(num,end=" ")
+
+
---
+++ Pyth.py
@@ -0,0 +1,13 @@
+#print("hello")
+s1='hello world'[::-1]
+print(s1)
+s='helloworld'
+# print(s.upper())
+# print(s.lower())
+# print(s.islower())
+# s1='A_L'
+# print(s1.islower())
+# print(s.capitalize())
+print(len(s))
+
+print(s.count('l'))
