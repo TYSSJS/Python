fdf46d9a917cfd917541da7e8a2359d5176208c6
unknown <anushanvi91@gmail.com>
2019-09-11 11:47:14 +0530

    Assignment files has been modified

---
+++ Assign2.py
@@ -0,0 +1,37 @@
+
+for num in range(1, 11):
+ print(num, end=' ')
+ print(" ")
+
+
+for num2 in range(5, 26):
+ print(num2, end=' ')
+ print(" ")
+
+ for num3 in range(5, 26, 5):
+  print(num3, end=' ')
+  print(" ")
+
+  for num4 in range(25, -1, -5):
+   print(num4, end=' ')
+   print(" ")
+
+   for num5 in range(2, 101, 2):
+    print(num5, end=' ')
+    print(" ")
+
+    for num6 in range(1, 100, 2):
+     print(num6, end=' ')
+     print(" ")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test1.py
@@ -0,0 +1,14 @@
+a = 10
+print(a)
+
+s='anusha'
+print(s.upper())
+print(s.lower())
+print(s.islower())
+print(s.isspace())
+print(s.isupper())
+print(s.capitalize())
+print(s.isspace())
+
+
+
